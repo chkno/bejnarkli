c7913a4fa3732f31ea8f0c870502510f5f0f120b
Chuck <chuck@intelligence.org>
2020-10-19 16:12:11 -0700

    Chain test pass/fail to text process exit status

--- BejnarkliTest.hs
+++ BejnarkliTest.hs
@@ -2,6 +2,8 @@ module Main where
 
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BL
+import Test.Framework as TF (defaultMain, testGroup, Test)
+import Test.Framework.Providers.QuickCheck2 (testProperty)
 import Test.QuickCheck (Property, quickCheck)
 import Test.QuickCheck.Instances.ByteString
 import Test.QuickCheck.Monadic (assert, monadicIO, run)
@@ -15,5 +17,9 @@ prop_BlobStoreWriteRead bs n b = monadicIO $ do
   assert $ ret == b
 
 main :: IO ()
-main = do
-  newBlobMap >>= quickCheck <$> prop_BlobStoreWriteRead
+main = tests >>= TF.defaultMain
+
+tests :: IO [Test]
+tests = do
+  m <- newBlobMap
+  pure [ testProperty "Map Write Read" (prop_BlobStoreWriteRead m) ]
--- default.nix
+++ default.nix
@@ -1,10 +1,19 @@
 { pkgs ? import <nixpkgs> { } }:
-pkgs.haskellPackages.callPackage
-({ base64-bytestring, errors, lib, mkDerivation, QuickCheck, quickcheck-instances, stdenv, utf8-string, }:
+pkgs.haskellPackages.callPackage ({ base64-bytestring, errors, lib, mkDerivation
+  , QuickCheck, quickcheck-instances, stdenv, test-framework
+  , test-framework-quickcheck2, utf8-string, }:
   mkDerivation {
     pname = "bejnarkli";
     version = "0.0.1.0";
     src = lib.cleanSource ./.;
-    testHaskellDepends = [ base64-bytestring errors QuickCheck quickcheck-instances utf8-string ];
+    testHaskellDepends = [
+      base64-bytestring
+      errors
+      QuickCheck
+      quickcheck-instances
+      test-framework
+      test-framework-quickcheck2
+      utf8-string
+    ];
     license = lib.licenses.mit;
   }) { }
