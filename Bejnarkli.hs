4ca94c2186add67f4a28e4dbcd5623fa57563c9f
Chuck <chuck@intelligence.org>
2020-10-19 16:08:39 -0700

    Use do.  seq doesn't for this

--- Bejnarkli.hs
+++ Bejnarkli.hs
@@ -2,6 +2,7 @@ module Bejnarkli
   ( BlobStore
   , getBlob
   , newBlobMap
+  , newBlobDir
   , someFunc
   , writeBlob
   ) where
@@ -31,7 +32,9 @@ newBlobMap = BlobMap <$> newIORef Map.empty
 instance BlobStore BlobMapStore where
   writeBlob (BlobMap rm) name blob  = let
       ename = ExtantBlob name
-      in pure $ seq (modifyIORef' rm (Map.insert ename blob)) ename
+      in do
+        modifyIORef' rm (Map.insert ename blob)
+        pure ename
   listBlobs (BlobMap rm)            = Map.keys <$> readIORef rm
   getBlob   (BlobMap rm) name       = do
     m <- readIORef rm
@@ -41,9 +44,13 @@ instance BlobStore BlobMapStore where
 
 data BlobDirStore = BlobDir FilePath
 newBlobDir :: FilePath -> IO BlobDirStore
-newBlobDir path = seq (createDirectoryIfMissing True path) (pure $ BlobDir path)
+newBlobDir path = do
+  createDirectoryIfMissing True path
+  pure $ BlobDir path
 instance BlobStore BlobDirStore where
-  writeBlob bd name blob = pure $ seq (BL.writeFile (blobFileName bd name) blob) (ExtantBlob name)
+  writeBlob bd name blob = do
+    BL.writeFile (blobFileName bd name) blob
+    pure $ ExtantBlob name
   listBlobs (BlobDir d)          = fmap ExtantBlob <$> mapMaybe unBlobFileName <$> listDirectory d
   getBlob   bd (ExtantBlob name) = BL.readFile (blobFileName bd name)
 
