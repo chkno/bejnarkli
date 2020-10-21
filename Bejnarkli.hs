7240f3b476d48391cb3a73c17c548d45a8165027
Chuck <chuck@intelligence.org>
2020-10-21 14:32:13 -0700

    Format with hindent

--- Bejnarkli.hs
+++ Bejnarkli.hs
@@ -9,16 +9,16 @@ module Bejnarkli
   , writeNamePrefixedBlob
   ) where
 
-import qualified Data.ByteString as BS
-import qualified Data.ByteString.Lazy as BL
-import Control.Exception (bracket)
 import Control.Error.Util (hush)
-import Data.ByteString.UTF8 (fromString, toString)
-import Data.Maybe (mapMaybe)
+import Control.Exception (bracket)
+import qualified Data.ByteString as BS
 import qualified Data.ByteString.Base64.URL as Base64
+import qualified Data.ByteString.Lazy as BL
+import Data.ByteString.UTF8 (fromString, toString)
 import Data.IORef
 import Data.Int (Int64)
 import qualified Data.Map.Strict as Map
+import Data.Maybe (mapMaybe)
 import System.Directory (createDirectoryIfMissing, listDirectory, renameFile)
 import System.FilePath
 import System.IO (hClose)
@@ -26,62 +26,69 @@ import System.IO.Temp (openBinaryTempFile)
 
 blobNameLength = 3
 
-newtype ExtantBlobName = ExtantBlob BS.ByteString deriving (Eq, Ord)
+newtype ExtantBlobName =
+  ExtantBlob BS.ByteString
+  deriving (Eq, Ord)
 
 class BlobStore a where
-  writeBlob             :: a -> BS.ByteString -> BL.ByteString -> IO ExtantBlobName
-  listBlobs             :: a -> IO [ExtantBlobName]
-  getBlob               :: a -> ExtantBlobName -> IO BL.ByteString
+  writeBlob :: a -> BS.ByteString -> BL.ByteString -> IO ExtantBlobName
+  listBlobs :: a -> IO [ExtantBlobName]
+  getBlob :: a -> ExtantBlobName -> IO BL.ByteString
   writeNamePrefixedBlob :: a -> BL.ByteString -> IO ExtantBlobName
-  writeNamePrefixedBlob bs stream = uncurry (writeBlob bs) $ strictPrefixSplitAt blobNameLength stream
+  writeNamePrefixedBlob bs stream =
+    uncurry (writeBlob bs) $ strictPrefixSplitAt blobNameLength stream
     where
+      strictPrefixSplitAt ::
+           Int64 -> BL.ByteString -> (BS.ByteString, BL.ByteString)
       -- |Like splitAt, but the prefix is strict
-      strictPrefixSplitAt :: Int64 -> BL.ByteString -> (BS.ByteString, BL.ByteString)
-      strictPrefixSplitAt i str = let tmp = BL.splitAt i str in (BL.toStrict (fst tmp), snd tmp)
+      strictPrefixSplitAt i str =
+        let tmp = BL.splitAt i str
+         in (BL.toStrict (fst tmp), snd tmp)
 
+newtype BlobMapStore =
+  BlobMap (IORef (Map.Map ExtantBlobName BL.ByteString))
 
-
-
-newtype BlobMapStore = BlobMap (IORef (Map.Map ExtantBlobName BL.ByteString))
 newBlobMap :: IO BlobMapStore
 newBlobMap = BlobMap <$> newIORef Map.empty
+
 instance BlobStore BlobMapStore where
-  writeBlob (BlobMap rm) name blob  = let
-      ename = ExtantBlob name
-      in do
-        modifyIORef' rm (Map.insert ename blob)
-        pure ename
-  listBlobs (BlobMap rm)            = Map.keys <$> readIORef rm
-  getBlob   (BlobMap rm) name       = do
+  writeBlob (BlobMap rm) name blob =
+    let ename = ExtantBlob name
+     in do modifyIORef' rm (Map.insert ename blob)
+           pure ename
+  listBlobs (BlobMap rm) = Map.keys <$> readIORef rm
+  getBlob (BlobMap rm) name = do
     m <- readIORef rm
     pure $ m Map.! name
 
+newtype BlobDirStore =
+  BlobDir FilePath
 
-
-newtype BlobDirStore = BlobDir FilePath
 newBlobDir :: FilePath -> IO BlobDirStore
 newBlobDir path = do
   createDirectoryIfMissing True $ path </> "incoming"
   pure $ BlobDir path
+
 instance BlobStore BlobDirStore where
-  writeBlob bd name blob = bracket
+  writeBlob bd name blob =
+    bracket
       (openBinaryTempFile (d </> "incoming") "new")
       (hClose . snd)
       (\(tmpname, tmpfile) -> do
-        BL.hPut tmpfile blob
-        renameFile tmpname (blobFileName bd name)
-        pure $ ExtantBlob name)
+         BL.hPut tmpfile blob
+         renameFile tmpname (blobFileName bd name)
+         pure $ ExtantBlob name)
     where
-    (BlobDir d) = bd
-  listBlobs (BlobDir d)          = fmap ExtantBlob . mapMaybe unBlobFileName <$> listDirectory d
-  getBlob   bd (ExtantBlob name) = BL.readFile (blobFileName bd name)
+      (BlobDir d) = bd
+  listBlobs (BlobDir d) =
+    fmap ExtantBlob . mapMaybe unBlobFileName <$> listDirectory d
+  getBlob bd (ExtantBlob name) = BL.readFile (blobFileName bd name)
 
 blobFileName :: BlobDirStore -> BS.ByteString -> FilePath
 blobFileName (BlobDir d) blobname = d </> toString (Base64.encode blobname)
 
 unBlobFileName :: FilePath -> Maybe BS.ByteString
 unBlobFileName relpath = hush $ Base64.decode $ fromString relpath
-
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
--- BejnarkliTest.hs
+++ BejnarkliTest.hs
@@ -4,25 +4,29 @@ import Control.Monad ((>=>))
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BL
 import System.IO.Temp (withSystemTempDirectory)
-import Test.Framework as TF (defaultMain, testGroup, Test)
+import Test.Framework as TF (Test, defaultMain, testGroup)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
-import Test.QuickCheck (Property, quickCheck, (==>))
+import Test.QuickCheck (Property, (==>), quickCheck)
 import Test.QuickCheck.Instances.ByteString
 import Test.QuickCheck.Monadic (assert, monadicIO, run)
 
 import Bejnarkli
 
-prop_BlobStoreWriteRead :: BlobStore bs => bs -> BS.ByteString -> BL.ByteString -> Property
-prop_BlobStoreWriteRead bs n b = BS.length n > 0 ==> monadicIO $ do
-  ename <- run $ writeBlob bs n b
-  ret <- run $ getBlob bs ename
-  assert $ ret == b
+prop_BlobStoreWriteRead ::
+     BlobStore bs => bs -> BS.ByteString -> BL.ByteString -> Property
+prop_BlobStoreWriteRead bs n b =
+  BS.length n > 0 ==> monadicIO $ do
+    ename <- run $ writeBlob bs n b
+    ret <- run $ getBlob bs ename
+    assert $ ret == b
 
-prop_BlobStoreWritePrefixedRead :: BlobStore bs => bs -> BL.ByteString -> Property
-prop_BlobStoreWritePrefixedRead bs stream = BL.length stream > blobNameLength ==> monadicIO $ do
-  ename <- run $ writeNamePrefixedBlob bs stream
-  ret <- run $ getBlob bs ename
-  assert $ ret == BL.drop blobNameLength stream
+prop_BlobStoreWritePrefixedRead ::
+     BlobStore bs => bs -> BL.ByteString -> Property
+prop_BlobStoreWritePrefixedRead bs stream =
+  BL.length stream > blobNameLength ==> monadicIO $ do
+    ename <- run $ writeNamePrefixedBlob bs stream
+    ret <- run $ getBlob bs ename
+    assert $ ret == BL.drop blobNameLength stream
 
 main :: IO ()
 main = withSystemTempDirectory "bej" $ tests >=> TF.defaultMain
@@ -31,8 +35,9 @@ tests :: FilePath -> IO [Test]
 tests tmpdir = do
   m <- newBlobMap
   d <- newBlobDir tmpdir
-  pure [ testProperty "Map Write Read" (prop_BlobStoreWriteRead m)
-       , testProperty "Map Write Prefixed Read" (prop_BlobStoreWritePrefixedRead m)
-       , testProperty "Dir Write Read" (prop_BlobStoreWriteRead d)
-       , testProperty "Dir Write Prefixed Read" (prop_BlobStoreWritePrefixedRead d)
-       ]
+  pure
+    [ testProperty "Map Write Read" (prop_BlobStoreWriteRead m)
+    , testProperty "Map Write Prefixed Read" (prop_BlobStoreWritePrefixedRead m)
+    , testProperty "Dir Write Read" (prop_BlobStoreWriteRead d)
+    , testProperty "Dir Write Prefixed Read" (prop_BlobStoreWritePrefixedRead d)
+    ]
