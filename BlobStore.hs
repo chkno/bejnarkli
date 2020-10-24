f1f46a862ac2222a729de5556130ed615e564c99
Chuck <chuck@intelligence.org>
2020-10-24 10:52:07 -0700

    Put BlobStore stuff in separate module

---
+++ BlobStore.hs
@@ -0,0 +1,170 @@
+{-# LANGUAGE TupleSections #-}
+
+module BlobStore
+  ( abort
+  , blobData
+  , blobName
+  , blobNameLength
+  , BlobStore
+  , commit
+  , getBlob
+  , listBlobs
+  , newBlobDir
+  , newBlobMap
+  , stageBlob
+  , writeNamePrefixedBlob
+  , writeTrustedBlob
+  , writeUntrustedBlob
+  ) where
+
+import Control.Exception (bracket)
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Base64.URL as Base64
+import qualified Data.ByteString.Lazy as BL
+import Data.ByteString.UTF8 (fromString, toString)
+import Data.Digest.Pure.SHA (bytestringDigest, hmacSha256)
+import Data.IORef (IORef, modifyIORef', newIORef, readIORef)
+import qualified Data.Map.Strict as Map
+import Data.Maybe (mapMaybe)
+import System.Directory
+  ( createDirectoryIfMissing
+  , listDirectory
+  , removeFile
+  , renameFile
+  )
+import System.FilePath ((</>))
+import System.IO (Handle, hClose)
+import System.IO.Temp (openBinaryTempFile)
+
+blobNameLength :: Int
+blobNameLength = 32
+
+newtype ExtantBlobName =
+  ExtantBlob BS.ByteString
+  deriving (Eq, Ord)
+
+blobName :: BS.ByteString -> BL.ByteString -> BS.ByteString
+blobName password blob =
+  BL.toStrict $ bytestringDigest $ hmacSha256 (BL.fromStrict password) blob
+
+-- |Lazily write a Lazy ByteString
+tee :: Handle -> BL.ByteString -> IO BL.ByteString
+tee h stream =
+  BL.fromChunks <$> mapM (\x -> BS.hPut h x >> pure x) (BL.toChunks stream)
+
+teeToTempFile ::
+     FilePath -> String -> BL.ByteString -> IO (BL.ByteString, FilePath)
+teeToTempFile dir template stream =
+  bracket
+    (openBinaryTempFile dir template)
+    (hClose . snd)
+    (\(tmpname, tmpfile) -> (, tmpname) <$> tee tmpfile stream)
+
+data StagedBlobHandle =
+  StagedBlobHandle
+    { blobData :: BL.ByteString
+    , commit :: BS.ByteString -> IO ExtantBlobName
+    , abort :: IO ()
+    }
+
+class BlobStore a where
+  stageBlob :: a -> BL.ByteString -> IO StagedBlobHandle
+  listBlobs :: a -> IO [ExtantBlobName]
+  getBlob :: a -> ExtantBlobName -> IO BL.ByteString
+
+writeNamePrefixedBlob ::
+     BlobStore bs
+  => bs
+  -> BS.ByteString
+  -> BL.ByteString
+  -> IO (Maybe ExtantBlobName)
+writeNamePrefixedBlob bs password stream =
+  let (name, blob) = strictPrefixSplitAt blobNameLength stream
+   in writeUntrustedBlob bs password name blob
+  where
+    strictPrefixSplitAt ::
+         Integral a => a -> BL.ByteString -> (BS.ByteString, BL.ByteString)
+    -- |Like splitAt, but the prefix is strict
+    strictPrefixSplitAt i str =
+      let tmp = BL.splitAt (fromIntegral i) str
+       in (BL.toStrict (fst tmp), snd tmp)
+
+newtype BlobMapStore =
+  BlobMap (IORef (Map.Map ExtantBlobName BL.ByteString))
+
+newBlobMap :: IO BlobMapStore
+newBlobMap = BlobMap <$> newIORef Map.empty
+
+instance BlobStore BlobMapStore where
+  stageBlob (BlobMap rm) blob =
+    pure
+      StagedBlobHandle
+        { blobData = blob
+        , commit =
+            \name ->
+              let ename = ExtantBlob name
+               in do modifyIORef' rm (Map.insert ename blob)
+                     pure ename
+        , abort = pure ()
+        }
+  listBlobs (BlobMap rm) = Map.keys <$> readIORef rm
+  getBlob (BlobMap rm) name = do
+    m <- readIORef rm
+    pure $ m Map.! name
+
+newtype BlobDirStore =
+  BlobDir FilePath
+
+newBlobDir :: FilePath -> IO BlobDirStore
+newBlobDir path = do
+  createDirectoryIfMissing True (path </> "incoming")
+  pure $ BlobDir path
+
+instance BlobStore BlobDirStore where
+  stageBlob bd blob = do
+    (teeWrappedBlob, tmpPath) <- teeToTempFile (d </> "incoming") "new" blob
+    pure
+      StagedBlobHandle
+        { blobData = teeWrappedBlob
+        , commit =
+            \name -> do
+              renameFile tmpPath (blobFileName bd name)
+              pure (ExtantBlob name)
+        , abort = removeFile tmpPath
+        }
+    where
+      (BlobDir d) = bd
+  listBlobs (BlobDir d) =
+    fmap ExtantBlob . mapMaybe unBlobFileName <$> listDirectory d
+  getBlob bd (ExtantBlob name) = BL.readFile (blobFileName bd name)
+
+blobFileName :: BlobDirStore -> BS.ByteString -> FilePath
+blobFileName (BlobDir d) blobname = d </> toString (Base64.encode blobname)
+
+unBlobFileName :: FilePath -> Maybe BS.ByteString
+unBlobFileName relpath =
+  case Base64.decode $ fromString relpath of
+    Left _ -> Nothing
+    Right n ->
+      if BS.length n == blobNameLength
+        then Just n
+        else Nothing
+
+writeTrustedBlob ::
+     BlobStore bs => bs -> BS.ByteString -> BL.ByteString -> IO ExtantBlobName
+writeTrustedBlob bs password blob = do
+  staged <- stageBlob bs blob
+  commit staged (blobName password (blobData staged))
+
+writeUntrustedBlob ::
+     BlobStore bs
+  => bs
+  -> BS.ByteString
+  -> BS.ByteString
+  -> BL.ByteString
+  -> IO (Maybe ExtantBlobName)
+writeUntrustedBlob bs password expectedHash blob = do
+  staged <- stageBlob bs blob
+  if expectedHash == blobName password (blobData staged)
+    then Just <$> commit staged expectedHash
+    else abort staged >> pure Nothing
---
+++ BlobStoreTest.hs
@@ -0,0 +1,117 @@
+module Main
+  ( main
+  ) where
+
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.ByteString.Lazy.UTF8 as U8L
+import qualified Data.ByteString.UTF8 as U8S
+import Data.Maybe (fromJust, isNothing)
+import System.Exit (ExitCode(ExitFailure, ExitSuccess), exitWith)
+import System.IO.Temp (withSystemTempDirectory)
+import Test.QuickCheck (Property, Result, isSuccess, quickCheckResult)
+import Test.QuickCheck.Instances.ByteString ()
+import Test.QuickCheck.Monadic (assert, monadicIO, run)
+
+import BlobStore
+  ( BlobStore
+  , blobName
+  , getBlob
+  , newBlobDir
+  , newBlobMap
+  , writeNamePrefixedBlob
+  , writeTrustedBlob
+  , writeUntrustedBlob
+  )
+
+password = U8S.fromString "test secret"
+
+prop_BlobStoreWriteReadTrusted ::
+     BlobStore bs => bs -> BL.ByteString -> Property
+prop_BlobStoreWriteReadTrusted bs b =
+  monadicIO $ do
+    ename <- run $ writeTrustedBlob bs password b
+    ret <- run $ getBlob bs ename
+    assert $ ret == b
+
+prop_BlobStoreWriteReadCorrectHash ::
+     BlobStore bs => bs -> BL.ByteString -> Property
+prop_BlobStoreWriteReadCorrectHash bs b =
+  monadicIO $ do
+    ename <- run $ writeUntrustedBlob bs password (blobName password b) b
+    ret <- run $ getBlob bs (fromJust ename)
+    assert $ ret == b
+
+prop_BlobStoreWriteWrongHash :: BlobStore bs => bs -> BL.ByteString -> Property
+prop_BlobStoreWriteWrongHash bs b =
+  monadicIO $
+  let wrongHash = blobName password (BL.append b (U8L.fromString "different"))
+   in do ename <- run $ writeUntrustedBlob bs password wrongHash b
+         assert $ isNothing ename
+
+prop_BlobStoreWriteWrongPassword ::
+     BlobStore bs => bs -> BL.ByteString -> Property
+prop_BlobStoreWriteWrongPassword bs b =
+  monadicIO $
+  let wrongHash = blobName (U8S.fromString "wrong password") b
+   in do ename <- run $ writeUntrustedBlob bs password wrongHash b
+         assert $ isNothing ename
+
+prop_BlobStoreWritePrefixedRead ::
+     BlobStore bs => bs -> BL.ByteString -> Property
+prop_BlobStoreWritePrefixedRead bs b =
+  let stream = BL.append (BL.fromStrict $ blobName password b) b
+   in monadicIO $ do
+        ename <- run $ writeNamePrefixedBlob bs password stream
+        ret <- run $ getBlob bs (fromJust ename)
+        assert $ ret == b
+
+prop_BlobStoreWritePrefixedWrongHash ::
+     BlobStore bs => bs -> BL.ByteString -> Property
+prop_BlobStoreWritePrefixedWrongHash bs b =
+  monadicIO $
+  let wrongHash = blobName password (BL.append b (U8L.fromString "different"))
+      stream = BL.append (BL.fromStrict wrongHash) b
+   in do ename <- run $ writeNamePrefixedBlob bs password stream
+         assert $ isNothing ename
+
+prop_BlobStoreWritePrefixedWrongPassword ::
+     BlobStore bs => bs -> BL.ByteString -> Property
+prop_BlobStoreWritePrefixedWrongPassword bs b =
+  monadicIO $
+  let wrongHash = blobName (U8S.fromString "wrong password") b
+      stream = BL.append (BL.fromStrict wrongHash) b
+   in do ename <- run $ writeNamePrefixedBlob bs password stream
+         assert $ isNothing ename
+
+tests :: IO [Result]
+tests =
+  withSystemTempDirectory
+    "bej"
+    (\tmpdir -> do
+       m <- newBlobMap
+       d <- newBlobDir tmpdir
+       mapM
+         quickCheckResult
+         [ prop_BlobStoreWriteReadTrusted m
+         , prop_BlobStoreWriteReadCorrectHash m
+         , prop_BlobStoreWriteWrongHash m
+         , prop_BlobStoreWriteWrongPassword m
+         , prop_BlobStoreWritePrefixedRead m
+         , prop_BlobStoreWritePrefixedWrongHash m
+         , prop_BlobStoreWritePrefixedWrongPassword m
+         , prop_BlobStoreWriteReadTrusted d
+         , prop_BlobStoreWriteReadCorrectHash d
+         , prop_BlobStoreWriteWrongHash d
+         , prop_BlobStoreWriteWrongPassword d
+         , prop_BlobStoreWritePrefixedRead d
+         , prop_BlobStoreWritePrefixedWrongHash d
+         , prop_BlobStoreWritePrefixedWrongPassword d
+         ])
+
+main :: IO ()
+main = do
+  allPassed <- and . fmap isSuccess <$> tests
+  exitWith
+    (if allPassed
+       then ExitSuccess
+       else ExitFailure 1)
