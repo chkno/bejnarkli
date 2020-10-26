e23821975d6e12a13ed09d8ad23675861c7175f7
Chuck <chuck@intelligence.org>
2020-10-26 14:15:41 -0700

    Server

--- Bejnarkli.hs
+++ Bejnarkli.hs
@@ -1,6 +1,25 @@
 module Bejnarkli
-  ( someFunc
+  ( bejnarkliServer
+  , someFunc
   ) where
+
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BL
+import Data.ByteString.Lazy.UTF8 (fromString)
+
+import BlobStore (BlobStore, writeNamePrefixedBlob)
+
+bejnarkliServer ::
+     BlobStore blobstore
+  => blobstore
+  -> BS.ByteString
+  -> BL.ByteString
+  -> IO BL.ByteString
+bejnarkliServer bs password stream = do
+  result <- writeNamePrefixedBlob bs password stream
+  case result of
+    Just _ -> pure $ fromString "y"
+    Nothing -> pure $ fromString "n"
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
--- BejnarkliTest.hs
+++ BejnarkliTest.hs
@@ -2,5 +2,38 @@ module Main
   ( main
   ) where
 
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.ByteString.Lazy.UTF8 as U8L
+import qualified Data.ByteString.UTF8 as U8S
+import System.Exit (ExitCode(ExitFailure, ExitSuccess), exitWith)
+import Test.QuickCheck (Property, Result, isSuccess, quickCheckResult)
+import Test.QuickCheck.Instances.ByteString ()
+import Test.QuickCheck.Monadic (assert, monadicIO, run)
+
+import Bejnarkli (bejnarkliServer)
+import BlobStore (blobName, getBlob, listBlobs, newBlobMap)
+
+password = U8S.fromString "test secret"
+
+prop_ServerWritesBlob :: BL.ByteString -> Property
+prop_ServerWritesBlob b =
+  let name = blobName password b
+      stream = BL.append (BL.fromStrict name) b
+   in monadicIO $ do
+        bs <- run newBlobMap
+        result <- run $ bejnarkliServer bs password stream
+        assert $ result == U8L.fromString "y"
+        storedBlobs <- run $ listBlobs bs
+        storedBlob <- run $ getBlob bs $ head storedBlobs
+        assert $ storedBlob == b
+
+tests :: IO [Result]
+tests = mapM quickCheckResult [prop_ServerWritesBlob]
+
 main :: IO ()
-main = someFunc
+main = do
+  allPassed <- and . fmap isSuccess <$> tests
+  exitWith
+    (if allPassed
+       then ExitSuccess
+       else ExitFailure 1)
