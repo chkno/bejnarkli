c6ac93e430505cdc30d4ca401e8779e8499b08f1
Chuck <chuck@intelligence.org>
2020-10-27 13:16:04 -0700

    Test final executable from the outside

--- default.nix
+++ default.nix
@@ -1,19 +1,23 @@
 { pkgs ? import <nixpkgs> { }, lint ? false, }:
 pkgs.haskellPackages.callPackage ({ base64-bytestring, hindent, hlint, lib
-  , memory, mkDerivation, network-simple, nixfmt, QuickCheck
-  , quickcheck-instances, SHA, stdenv, temporary, utf8-string, }:
+  , memory, mkDerivation, network-simple, nixfmt, openssl, QuickCheck
+  , quickcheck-instances, SHA, socat, stdenv, temporary, utf8-string, }:
   mkDerivation {
     pname = "bejnarkli";
     version = "0.0.1.0";
     src = lib.cleanSource ./.;
     libraryHaskellDepends =
       [ base64-bytestring memory network-simple SHA temporary utf8-string ];
-    testHaskellDepends = [ QuickCheck quickcheck-instances ]
+    testHaskellDepends = [ openssl QuickCheck quickcheck-instances socat ]
       ++ lib.optionals lint [ hindent hlint nixfmt ];
     postCheck = lib.optionalString lint ''
       hlint *.hs
       hindent --validate *.hs
       nixfmt --check *.nix
+    '';
+    postInstall = ''
+      patchShebangs test.sh
+      ./test.sh $out/bin/bejnarkli
     '';
     license = lib.licenses.mit;
   }) { }
---
+++ test.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+bejnarkli=${1:-$PWD/dist-newstyle/build/*/*/bejnarkli-*/x/bejnarkli/build/bejnarkli/bejnarkli}
+
+port=8934
+outdir=teh-blobs
+password=secret
+payload="Test content"
+max_attempts=10
+delay_between_attempts=.2
+
+message() {
+  printf '%s' "$payload" | openssl dgst -sha256 -binary -hmac "$password"
+  printf '%s' "$payload"
+}
+
+send() {
+  socat - "TCP4:localhost:$port"
+}
+
+
+tmpdir=
+bejnarkli_pid=
+cleanup() {
+  if [[ "$bejnarkli_pid" ]];then
+    kill "$bejnarkli_pid"
+  fi
+  if [[ "$tmpdir" && -e "$tmpdir" ]];then
+    rm -rf "$tmpdir"
+  fi
+}
+trap cleanup EXIT
+
+tmpdir=$(mktemp -d)
+cd "$tmpdir"
+
+$bejnarkli &
+bejnarkli_pid=$!
+
+attempts=0
+until [[ "$(message | send)" == y ]];do
+  if (( attempts++ > max_attempts ));then
+    exit 1
+  fi
+  sleep "$delay_between_attempts"
+done
+
+captured_blob_data=$(find "$outdir" -name incoming -prune -o -type f -exec cat {} +)
+
+[[ "$captured_blob_data" == "$payload" ]]
+
+echo "PASS"
