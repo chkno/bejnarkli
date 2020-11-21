d137cfe696140a4beb0062da72321e5bc91d7ee3
Chuck <chuck@intelligence.org>
2020-11-20 16:58:28 -0800

    Don't use getChanContents.  It uses unsafeInterleaveIO inside

--- RetryQueue.hs
+++ RetryQueue.hs
@@ -1,11 +1,9 @@
-{-# LANGUAGE ScopedTypeVariables #-}
-
 module RetryQueue
   ( mapChanWithBackoff
   ) where
 
 import Control.Concurrent (ThreadId, forkIO, threadDelay)
-import Control.Concurrent.Chan (Chan, getChanContents, writeChan)
+import Control.Concurrent.Chan (Chan, readChan, writeChan)
 import Control.Exception (try)
 import System.Random (getStdRandom, randomR)
 
@@ -15,25 +13,19 @@ import System.Random (getStdRandom, randomR)
 --   * Subsequent calls of f are delayed by exponential back-off with jitter
 --     until successful again.
 mapChanWithBackoff ::
-     forall a.
-     Float
-  -> Float
-  -> Float
-  -> (a -> IO Bool)
-  -> Chan a
-  -> IO ThreadId
+     Float -> Float -> Float -> (a -> IO Bool) -> Chan a -> IO ThreadId
 mapChanWithBackoff increment minDelay maxDelay f chan =
-  forkIO $ getChanContents chan >>= process minDelay
+  forkIO $ process minDelay
   where
-    process :: Float -> [a] -> IO ()
-    process prevBackoff (item:next) = do
+    process :: Float -> IO ()
+    process prevBackoff = do
+      item <- readChan chan
       success <- try $ f item :: IO (Either IOError Bool)
       let backoff = max minDelay (min maxDelay (prevBackoff * increment))
        in if success == Right True
-            then process minDelay next
+            then process minDelay
             else do
               delay <- getStdRandom (randomR (0, 2 * backoff))
               threadDelay $ round $ 100000 * delay
               writeChan chan item
-              process backoff next
-    process _ [] = error "getChanContents unexpectedly ended"
+              process backoff
