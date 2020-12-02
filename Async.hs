e021fa55b49ffe4e6db65481b1d40f63275e4b9d
Chuck <chuck@intelligence.org>
2020-12-02 13:24:55 -0800

    Send to a specified number of servers

---
+++ Async.hs
@@ -0,0 +1,34 @@
+module Async
+  ( atLeast
+  , atLeast_
+  ) where
+
+import Control.Concurrent.Async (Async, async, cancel, waitAny, waitAnyCancel)
+
+-- Like `filter (not p)`, but always remove exactly one element
+removeOne :: (a -> Bool) -> [a] -> Maybe [a]
+removeOne _ [] = Nothing
+removeOne p (x:xs)
+  | p x = Just xs
+  | otherwise = (x :) <$> removeOne p xs
+
+waitAtLeast :: Int -> [Async a] -> IO [a]
+waitAtLeast 0 actions = mapM_ cancel actions >> pure []
+waitAtLeast 1 actions = waitAnyCancel actions >>= \(_, result) -> pure [result]
+waitAtLeast n actions = do
+  (action, result) <- waitAny actions
+  case removeOne (== action) actions of
+    Just remainingActions -> (result :) <$> waitAtLeast (n - 1) remainingActions
+    Nothing ->
+      error
+        "Async Eq doesn't work like I assumed.  Change this to keep its own indicies"
+
+-- Like `sequence`, but finish when n actions finish.  The remaining actions are cancelled.
+-- Like `race` from async, but for more than two actions
+atLeast :: Int -> [IO a] -> IO [a]
+atLeast n actions = mapM async actions >>= waitAtLeast n
+
+-- Like `sequence_`, but finish when n actions finish.  The remaining actions are cancelled.
+-- Like `race` from async, but for more than two actions
+atLeast_ :: Int -> [IO a] -> IO ()
+atLeast_ n actions = atLeast n actions >> pure ()
--- MainSend.hs
+++ MainSend.hs
@@ -4,9 +4,12 @@ module Main
 
 import Conduit ((.|), runConduitRes, sourceFile)
 import Data.ByteString.UTF8 (fromString)
+import Data.Maybe (fromMaybe)
 import Options.Applicative
   ( Parser
+  , ReadM
   , (<**>)
+  , auto
   , execParser
   , fullDesc
   , header
@@ -15,6 +18,9 @@ import Options.Applicative
   , info
   , long
   , metavar
+  , option
+  , optional
+  , readerError
   , short
   , some
   , strArgument
@@ -22,20 +28,35 @@ import Options.Applicative
   )
 import Options.Applicative.Types (ParserInfo)
 
+import Async (atLeast_)
 import Bejnarkli (defaultPort)
 import BlobStore (Password(Pass), blobName)
 import TCPClient (retryingTCPClient)
 
 data Args =
   Args
-    { password :: String
+    { durability :: Maybe Int
+    , password :: String
     , file :: FilePath
     , servers :: [String]
     }
 
+validDurability :: ReadM Int
+validDurability = do
+  i <- auto
+  if i > 0
+    then pure i
+    else readerError "Durability must be greater than 0"
+
 parser :: Parser Args
 parser =
-  Args <$> strOption (long "password" <> short 'p' <> help "Transfer password") <*>
+  Args <$>
+  optional
+    (option
+       validDurability
+       (long "durability" <>
+        short 'n' <> help "Exit after successfully sending to this many servers")) <*>
+  strOption (long "password" <> short 'p' <> help "Transfer password") <*>
   strArgument (metavar "FILE" <> help "File containing the blob to be sent") <*>
   some (strArgument (metavar "SERVERS..." <> help "Server to send the file to"))
 
@@ -51,7 +72,8 @@ main = do
   hash <-
     runConduitRes $
     sourceFile (file args) .| blobName (Pass (fromString (password args)))
-  mapM_
-    (\server ->
-       retryingTCPClient defaultPort server hash (sourceFile (file args)))
-    (servers args)
+  atLeast_ (fromMaybe (length (servers args)) (durability args)) $
+    map
+      (\server ->
+         retryingTCPClient defaultPort server hash (sourceFile (file args)))
+      (servers args)
